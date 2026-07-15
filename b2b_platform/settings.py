e65674a3451cc7da8b9a71073ea9097f32fc2e38
Atunde Toheeb Ayomide <atundetoheeb1@gmail.com>
2026-07-15 13:34:27 +0100

    change debug to false

--- b2b_platform/settings.py
+++ b2b_platform/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-use-env-variable'
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 CSRF_TRUSTED_ORIGINS = ['https://b2b-sales-platform-production.up.railway.app']
